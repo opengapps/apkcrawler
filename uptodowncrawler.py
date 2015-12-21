08892e995fa1d5494344453131c713eaed081cac
mfonville <maarten.fonville@gmail.com>
2015-12-21 10:35:23 +0100

    Add UpToDown dictionary

--- uptodowncrawler.py
+++ uptodowncrawler.py
@@ -53,7 +53,64 @@ logFile   = '{0}.log'.format(os.path.basename(sys.argv[0]))
 logLevel  = (logging.DEBUG if Debug.DEBUG else logging.INFO)
 logFormat = '%(asctime)s %(levelname)s/%(funcName)s(%(process)-5d): %(message)s'
 
-allUpToDownNames ={'com.google.android.apps.maps': 'google-maps'}
+allUpToDownNames = {
+    #com.android.chrome #they only have chrome beta
+    'com.android.vending' : 'google-play',
+    #com.google.android.androidforwork
+    'com.google.android.apps.books': 'google-play-books',
+    #com.google.android.apps.cloudprint
+    'com.google.android.apps.docs': 'google-drive',
+    'com.google.android.apps.docs.editors.docs': 'google-docs',
+    'com.google.android.apps.docs.editors.sheets': 'google-sheets',
+    'com.google.android.apps.docs.editors.slides': 'google-slides',
+    #com.google.android.apps.enterprise.dmagent
+    'com.google.android.apps.fitness': 'google-fit',
+    #com.google.android.apps.gcs
+    #com.google.android.apps.genie.geniewidget
+    'com.google.android.apps.inbox': 'inbox-by-gmail',
+    #com.google.android.apps.inputmethod.hindi
+    #com.google.android.apps.inputmethod.zhuyin
+    'com.google.android.apps.magazines': 'google-play-newsstand',
+    'com.google.android.apps.maps': 'google-maps',
+    #com.google.android.apps.mediashell
+    'com.google.android.apps.messaging': 'messenger',
+    #com.google.android.apps.photos
+    'com.google.android.apps.plus': 'google-plus',
+    'com.google.android.apps.translate': 'traductor-de-google',
+    #com.google.android.apps.tycho
+    #com.google.android.apps.walletnfcrel
+    #com.google.android.calculator
+    'com.google.android.calendar': 'google-calendar',
+    #com.google.android.contacts
+    'com.google.android.deskclock': 'clock',
+    #com.google.android.dialer
+    'com.google.android.ears': 'sound-search-for-google-play',
+    'com.google.android.gm': 'gmail',
+    #com.google.android.gm.exchange
+    'com.google.android.gms': 'google-play-services',
+    'com.google.android.googlecamera': 'google-camera',
+    'com.google.android.googlequicksearchbox': 'google-search',
+    'com.google.android.inputmethod.japanese': 'google-japanese-input',
+    #com.google.android.inputmethod.korean
+    'com.google.android.inputmethod.latin': 'google-keyboard',
+    #com.google.android.inputmethod.pinyin
+    #com.google.android.katniss
+    'com.google.android.keep': 'google-keep',
+    'com.google.android.launcher': 'google-now-launcher',
+    #com.google.android.leanbacklauncher
+    'com.google.android.marvin.talkback': 'google-talkback',
+    'com.google.android.music': 'google-play-music',
+    'com.google.android.play.games': 'google-play-games',
+    'com.google.android.street': 'street-view-on-google-maps',
+    'com.google.android.talk': 'hangouts',
+    #com.google.android.tts
+    #com.google.android.tv
+    #com.google.android.tv.remote
+    'com.google.android.videos': 'google-play-movies',
+    'com.google.android.webview': 'android-system-webview',
+    'com.google.android.youtube': 'youtube',
+    #com.google.android.youtube.tv
+    'com.google.earth': 'google-earth'}
 
 def downloadApk(url,package):
     """
@@ -101,33 +158,35 @@ def checkOneApp(apkid):
 
     logging.info('Checking app: {0}'.format(apkid))
 
-    upToDownName = allUpToDownNames[apkid]
-    html_name = '{0}.html'.format(upToDownName)
-    url       = 'http://' + upToDownName + '.en.uptodown.com/android/download'
-    html      = Debug.readFromFile(html_name)
-
-    if html == '':
-        session = requests.Session()
-        session.proxies = Debug.getProxy()
-        logging.debug('Requesting: ' + url)
-        resp    = session.get(url)
-        html    = unicodedata.normalize('NFKD', resp.text).encode('ascii', 'ignore')
-        Debug.writeToFile(html_name, html, resp.encoding)
-
     try:
-        dom       = BeautifulSoup(html, 'html5lib')
-        latestapk = dom.findAll('iframe', {'id': 'iframe_download'})[0]['src'] #note that this url will still result in a redirect 302
+        upToDownName = allUpToDownNames[apkid]
+        html_name = '{0}.html'.format(upToDownName)
+        url       = 'http://' + upToDownName + '.en.uptodown.com/android/download'
+        html      = Debug.readFromFile(html_name)
 
-        #We still miss versioncode comparison here
-        downloadApk(latestapk,apkid)
+        if html == '':
+            session = requests.Session()
+            session.proxies = Debug.getProxy()
+            logging.debug('Requesting: ' + url)
+            resp    = session.get(url)
+            html    = unicodedata.normalize('NFKD', resp.text).encode('ascii', 'ignore')
+            Debug.writeToFile(html_name, html, resp.encoding)
 
-        #We still miss fetching older versions
+        try:
+            dom       = BeautifulSoup(html, 'html5lib')
+            latestapk = dom.findAll('iframe', {'id': 'iframe_download'})[0]['src'] #note that this url will still result in a redirect 302
 
-    except IndexError:
-        logging.info('{0} not supported by uptodown.com ...'.format(apkid))
-    except:
-        logging.exception('!!! Error parsing html from: "{0}"'.format(url))
+            #We still miss versioncode comparison here
+            downloadApk(latestapk,apkid)
 
+            #We still miss fetching older versions
+
+        except IndexError:
+            logging.info('{0} not supported by uptodown.com ...'.format(apkid))
+        except:
+            logging.exception('!!! Error parsing html from: "{0}"'.format(url))
+    except KeyError:
+        logging.info('{0} not in uptodown.com dictionary...'.format(apkid))
 # END: def checkOneApp:
 
 
