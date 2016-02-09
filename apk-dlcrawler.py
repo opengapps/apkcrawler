fa4722c2331f149e8e5e8db3f5782f62121f8994
mfonville <maarten.fonville@gmail.com>
2016-02-09 13:05:01 +0100

    Made ApkdlCrawler a class

--- apk-dlcrawler.py
+++ apk-dlcrawler.py
@@ -44,172 +44,193 @@ else:
 
 manager = multiprocessing.Manager()
 Global  = manager.Namespace()
-Global.report = None
 
 # logging
 logFile   = '{0}.log'.format(os.path.basename(__file__))
 logLevel  = (logging.DEBUG if Debug.DEBUG else logging.INFO)
 logFormat = '%(asctime)s %(levelname)s/%(funcName)s(%(process)-5d): %(message)s'
 
+class ApkdlCrawler(object):
+    def __init__(self, report, dlFiles=[], dlFilesBeta=[]):
+        self.report      = report
+        self.dlFiles     = dlFiles
+        self.dlFilesBeta = dlFilesBeta
 
-def getUrlFromRedirect(apkname, url):
-    """
-    getUrlFromRedirect(url):
-    """
-    link      = ''
+    def getUrlFromRedirect(self, apkname, url):
+        """
+        getUrlFromRedirect(url):
+        """
+        link      = ''
 
-    session = requests.Session()
-    session.proxies = Debug.getProxy()
-    logging.debug('Requesting2: ' + url)
-    resp    = session.get(url)
-    html    = unicodedata.normalize('NFKD', resp.text).encode('ascii', 'ignore')
-
-    try:
-        dom  = BeautifulSoup(html, 'html5lib')
-        link = dom.find('span', {'class': 'glyphicon glyphicon-cloud-download'}).parent['href']
-    except:
-        logging.exception('!!! Error parsing html from: "{0}"'.format(url))
-
-    return link
-# END: def getUrlFromRedirect:
-
-
-def downloadApk(apkInfo):
-    """
-    downloadApk(apkInfo): Download the specified URL to APK file name
-    """
-    apkname = '{0}_{1}-{2}_minAPI{3}.apk'.format(apkInfo.name,
-                                                 apkInfo.ver,
-                                                 apkInfo.vercode,
-                                                 apkInfo.sdk)
-
-    url     = getUrlFromRedirect(apkname, apkInfo.download_src)
-    if url == '':
-        logging.error('Unable to determine redirect url for ' + apkname)
-        return
-
-    logging.info('Downloading "{0}" from: {1}'.format(apkname,url))
-
-    try:
-        if os.path.exists(apkname):
-            logging.info('Downloaded APK already exists.')
-            return
-
-        if os.path.exists(os.path.join('.', 'apkcrawler', apkname)):
-            logging.info('Downloaded APK already exists (in ./apkcrawler/).')
-            return
-
-        if os.path.exists(os.path.join('..', 'apkcrawler', apkname)):
-            logging.info('Downloaded APK already exists (in ../apkcrawler/).')
-            return
-
-        # Open the url
         session = requests.Session()
         session.proxies = Debug.getProxy()
-        r = session.get(url)
+        logging.debug('Requesting2: ' + url)
+        resp    = session.get(url)
+        html    = unicodedata.normalize('NFKD', resp.text).encode('ascii', 'ignore')
 
-        with open(apkname, 'wb') as local_file:
-            local_file.write(r.content)
-        print('{0} '.format(apkname)),
-        sys.stdout.flush()
-    except OSError:
-        logging.exception('!!! Filename is not valid: "{0}"'.format(apkname))
-# END: def downloadApk
+        try:
+            dom  = BeautifulSoup(html, 'html5lib')
+            link = dom.find('span', {'class': 'glyphicon glyphicon-cloud-download'}).parent['href']
+        except:
+            logging.exception('!!! Error parsing html from: "{0}"'.format(url))
 
-
-def checkOneApp(apkid):
-    """
-    checkOneApp(apkid):
-    """
-    logging.info('Checking app: {0}'.format(apkid))
-
-    url       = 'http://apk-dl.com/' + apkid
-
-    session = requests.Session()
-    session.proxies = Debug.getProxy()
-    logging.debug('Requesting: ' + url)
-    resp    = session.get(url)
-    html    = unicodedata.normalize('NFKD', resp.text).encode('ascii', 'ignore')
-
-    try:
-        dom     = BeautifulSoup(html, 'html5lib')
-        apklist = dom.findAll('ul', {'class': 'apks dlist'})[0]
-        apks    = apklist.findAll('div', {'class': 'details'})
-
-        for apk in apks:
-            items = apk.findAll('div')
-            dApk = {}
-            for item in items:
-                itext = '{0}'.format(item.get_text().encode('ascii', 'ignore'))
-                itext = re.sub('\s', '', itext)
-                itextsp = itext.split(':', 1)
-                if len(itextsp) == 2:
-                    dApk[str(itextsp[0])] = str(itextsp[1])
-            apkurl = apk.find('a', {'class': 'btn btn-success'})
-            if apkurl:
-                dApk['url'] = 'http:' + apkurl['href']
-
-                Debug.printDictionary(dApk)
-
-                if 'Version' in dApk and 'RequiresAndroid' in dApk:
-                    (trash, sdk) = dApk['RequiresAndroid'].split('API:', 1)
-                    sdk = sdk[0:-1]
-                    (ver, vercode) = dApk['Version'].split('(Code:', 1)
-                    ver     = ver.split('(', 1)[0].strip()
-                    vercode = vercode[0:-1].strip()
-
-                    avi = ApkVersionInfo(name=apkid,
-                                         sdk=sdk,
-                                         ver=ver,
-                                         vercode=vercode,
-                                         download_src = dApk['url']
-                                         )
-
-                    if Global.report.isThisApkNeeded(avi):
-                        downloadApk(avi)
-
-    except IndexError:
-        logging.info('{0} not supported by apk-dl.com ...'.format(apkid))
-    except:
-        logging.exception('!!! Error parsing html from: "{0}"'.format(url))
-
-# END: def checkOneApp:
+        return link
+    # END: def getUrlFromRedirect:
 
 
-def main(param_list):
-    """
-    main(): single parameter for report_sources.sh output
-    """
-    lines = ''
-    if len(param_list) == 1:
-        with open(param_list[0]) as report:
-            lines = report.readlines()
-    else:
-        lines = sys.stdin.readlines()
+    def downloadApk(self, apkInfo, isBeta=False):
+        """
+        downloadApk(apkInfo): Download the specified URL to APK file name
+        """
+        apkname = '{0}_{1}-{2}_minAPI{3}.apk'.format(apkInfo.name,
+                                                     apkInfo.ver,
+                                                     apkInfo.vercode,
+                                                     apkInfo.sdk)
 
-    Global.report = ReportHelper(lines)
-    keys = Global.report.dAllApks.keys()
+        url     = self.getUrlFromRedirect(apkname, apkInfo.download_src)
+        if url == '':
+            logging.error('Unable to determine redirect url for ' + apkname)
+            return
 
-    if len(keys) == 0:
-        print('ERROR: expecting:')
-        print(' - 1 parameter (report file from output of report_sources.sh)')
-        print(' or ')
-        print(' - stdin from report_sources.sh')
-        return
+        logging.info('Downloading "{0}" from: {1}'.format(apkname,url))
 
-    # Start checking all apkids ...
-    p = multiprocessing.Pool(5)
-    p.map(checkOneApp, keys)
+        try:
+            if os.path.exists(apkname):
+                logging.info('Downloaded APK already exists.')
+                return
 
-# END: main():
+            if os.path.exists(os.path.join('.', 'apkcrawler', apkname)):
+                logging.info('Downloaded APK already exists (in ./apkcrawler/).')
+                return
+
+            if os.path.exists(os.path.join('..', 'apkcrawler', apkname)):
+                logging.info('Downloaded APK already exists (in ../apkcrawler/).')
+                return
+
+            # Open the url
+            session = requests.Session()
+            session.proxies = Debug.getProxy()
+            r = session.get(url)
+
+            with open(apkname, 'wb') as local_file:
+                local_file.write(r.content)
+            if isBeta:
+                self.dlFilesBeta.append(apkname)
+                logging.debug('beta: ' + ', '.join(self.dlFilesBeta))
+            else:
+                self.dlFiles.append(apkname)
+                logging.debug('reg : ' + ', '.join(self.dlFiles))
+        except OSError:
+            logging.exception('!!! Filename is not valid: "{0}"'.format(apkname))
+    # END: def downloadApk
+
+
+    def checkOneApp(self, apkid):
+        """
+        checkOneApp(apkid):
+        """
+        logging.info('Checking app: {0}'.format(apkid))
+
+        url       = 'http://apk-dl.com/' + apkid
+
+        session = requests.Session()
+        session.proxies = Debug.getProxy()
+        logging.debug('Requesting: ' + url)
+        resp    = session.get(url)
+        html    = unicodedata.normalize('NFKD', resp.text).encode('ascii', 'ignore')
+
+        try:
+            dom     = BeautifulSoup(html, 'html5lib')
+            apklist = dom.findAll('ul', {'class': 'apks dlist'})[0]
+            apks    = apklist.findAll('div', {'class': 'details'})
+
+            for apk in apks:
+                items = apk.findAll('div')
+                dApk = {}
+                for item in items:
+                    itext = '{0}'.format(item.get_text().encode('ascii', 'ignore'))
+                    itext = re.sub('\s', '', itext)
+                    itextsp = itext.split(':', 1)
+                    if len(itextsp) == 2:
+                        dApk[str(itextsp[0])] = str(itextsp[1])
+                apkurl = apk.find('a', {'class': 'btn btn-success'})
+                if apkurl:
+                    dApk['url'] = 'http:' + apkurl['href']
+
+                    Debug.printDictionary(dApk)
+
+                    if 'Version' in dApk and 'RequiresAndroid' in dApk:
+                        (trash, sdk) = dApk['RequiresAndroid'].split('API:', 1)
+                        sdk = sdk[0:-1]
+                        (ver, vercode) = dApk['Version'].split('(Code:', 1)
+                        ver     = ver.split('(', 1)[0].strip()
+                        vercode = vercode[0:-1].strip()
+
+                        avi = ApkVersionInfo(name=apkid,
+                                             sdk=sdk,
+                                             ver=ver,
+                                             vercode=vercode,
+                                             download_src = dApk['url']
+                                             )
+
+                        if self.report.isThisApkNeeded(avi):
+                            self.downloadApk(avi)
+
+        except IndexError:
+            logging.info('{0} not supported by apk-dl.com ...'.format(apkid))
+        except:
+            logging.exception('!!! Error parsing html from: "{0}"'.format(url))
+    # END: def checkOneApp:
+
+    def crawl(self, threads=5):
+        """
+        crawl(): check all apk-dl apps
+        """
+        # Start checking all apkids ...
+        p = multiprocessing.Pool(threads)
+        p.map(unwrap_self_checkOneApp, zip([self]*len(report.dAllApks.keys()), report.dAllApks.keys()))
+    # END: crawl():
+# END: class ApkdlCrawler
+
+def unwrap_self_checkOneApp(arg, **kwarg):
+    return ApkdlCrawler.checkOneApp(*arg, **kwarg)
 
 ###################
 # END: Functions  #
 ###################
 
 if __name__ == "__main__":
+    """
+    main(): single parameter for report_sources.sh output
+    """
     logging.basicConfig(filename = logFile, filemode = 'w', level = logLevel, format = logFormat)
     logging.getLogger("requests").setLevel(logging.WARNING)
     logging.getLogger("requesocks").setLevel(logging.WARNING)
 
-    main(sys.argv[1:])
+    lines = ''
+    if len(sys.argv[1:]) == 1:
+        with open(sys.argv[1:]) as report:
+            lines = report.readlines()
+    else:
+        lines = sys.stdin.readlines()
+
+    report = ReportHelper(lines)
+
+    if len(report.dAllApks.keys()) == 0:
+        print('ERROR: expecting:')
+        print(' - 1 parameter (report file from output of report_sources.sh)')
+        print(' or ')
+        print(' - stdin from report_sources.sh')
+        exit(1)
+
+    crawler = ApkdlCrawler(report)
+    crawler.crawl()
+    logging.debug('Just before outputString creation')
+    outputString = ' '.join(crawler.dlFiles)
+    if crawler.dlFilesBeta:
+        outputString += ' beta ' + ' '.join(crawler.dlFilesBeta)
+    logging.debug('Just after outputString creation')
+    if outputString:
+        print(outputString)
+        sys.stdout.flush()
+    logging.debug('Done ...')
