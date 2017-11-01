734a8266eba812dcf5a8504fc6f6e7fec6e426e5
therealssj <mehul.guptagm@gmail.com>
2017-11-01 18:47:32 +0530

    Use encrypted password for login

--- googleplayapi/googleplay.py
+++ googleplayapi/googleplay.py
@@ -9,6 +9,10 @@ import pprint
 import http.client
 import requests
 
+import hashlib
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
 from google.protobuf import descriptor
 from google.protobuf.internal.containers import RepeatedCompositeFieldContainer
 from google.protobuf import text_format
@@ -35,6 +39,7 @@ class RequestError(Exception):
 
 class RequestResult(object):
     """RequestResult"""
+
     def __init__(self, status_code=None, body=None):
         self.status_code = status_code
         self.body = body
@@ -43,6 +48,70 @@ class RequestResult(object):
 class GooglePlayApplication:
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True)
+
+
+class GooglePlayPassword:
+    """
+    Python implementation of the java class to encrypt the login password.
+
+    initialization: email and password of target account.
+
+    getEncryptedPassword() can be used to directly get the encrypted password string.
+
+    encryptString() can be used to encrypt any string.
+    """
+    KEY = "AAAAgMom/1a/v0lblO2Ubrt60J2gcuXSljGFQXgcyZWveWLEwo6prwgi3iJIZdodyhKZQrNWp5nKJ3srRXcUW+F1BD3baEVGcmEgqaLZUNBjm057pKRI16kB0YppeGx5qIQ5QjKzsR8ETQbKLNWgRY0QRNVz34kMJR3P/LgHax/6rmf5AAAAAwEAAQ=="
+
+    def __init__(self, email=None, password=None):
+        self.password_string = email + '\u0000' + password
+
+    def getEncryptedPassword(self):
+        return self.encryptString(self.password_string)
+
+    def readInt(self, bArr, i):
+        return (((((bArr[i]&255) << 24)|0)|((bArr[i + 1]&255) << 16))|(
+            (bArr[i + 2]&255) << 8))|(bArr[i + 3]&255)
+
+    def createKeyFromString(self, str, bArr):
+        decode = base64.standard_b64decode(str)
+        keyint = self.readInt(decode, 0)
+        obj = decode[4:(4 + keyint)]
+        bigInt = int.from_bytes(obj, byteorder='big')
+        keyInt2 = self.readInt(decode, keyint + 4)
+        obj2 = decode[keyint + 8:keyint + 8 + keyInt2]
+        bigInt2 = int.from_bytes(obj2, byteorder='big')
+        decode = hashlib.sha1(decode).digest()
+        bArr[1:] = decode[:4]
+        rsaKey = RSA.construct((bigInt, bigInt2))
+        return rsaKey, bArr
+
+    def encryptString(self, inputstr):
+        i = 0
+        obj = bytearray(5)
+        rsaKey, obj = self.createKeyFromString(self.KEY, obj)
+        if rsaKey is None:
+            return ""
+
+        try:
+            bytes = inputstr.encode('utf-8')
+            bLen = int(((len(bytes) - 1) / 86)) + 1
+            obj2 = bytearray(bLen * 133)
+            encryptor = PKCS1_OAEP.new(rsaKey)
+            while i < bLen:
+                offset = i * 86
+                strlen = (len(bytes) - offset if i == (
+                bLen - 1) else 86) + offset
+
+                encstr = encryptor.encrypt(bytes[offset:strlen])
+                obj2[i * 133:i * 133 + len(obj)] = obj
+                obj2[
+                i * 133 + len(obj):i * 133 + len(obj) + len(encstr)] = encstr
+                i += 1
+
+            return base64.urlsafe_b64encode(obj2)
+        except Exception as e:
+            print(e)
+            return ""
 
 
 class GooglePlayAPI(object):
@@ -74,8 +143,8 @@ class GooglePlayAPI(object):
         # The versionCode is the vercode of the Play Store app
         self.downloadUserAgent = "AndroidDownloadManager/7.1 (Linux; U; Android 7.1; Pixel Build/NZZ99Z)"
         self.regionCookie = "US"
-        self.defaultAgentvername="7.0.12.H-all [0]"
-        self.defaultAgentvercode="80701200"
+        self.defaultAgentvername = "7.0.12.H-all [0]"
+        self.defaultAgentvercode = "80701200"
         # self.proxy_dict = {
         #         "http"  : "http://81.137.100.158:8080",
         #         "https" : "http://81.137.100.158:8080",
@@ -134,23 +203,26 @@ class GooglePlayAPI(object):
             if (email is None or password is None):
                 logging.error('{0} Needs a authSubToken or (email and password)'.format(self.androidId))
             else:
-                params = {"Email": email,
-                          "Passwd": password,
-                          "service": "androidmarket",
-                          "accountType": self.ACCOUNT_TYPE_HOSTED_OR_GOOGLE,
-                          "has_permission": "1",
-                          "source": "android",
-                          "androidId": self.androidId,
-                          "app": "com.android.vending",
-                          # "client_sig": self.client_sig,
-                          "device_country": "us",
-                          "operatorCountry": "us",
-                          "lang": "us",
-                          "sdk_version": "17"}  # to work around oauth issues
+                params = {
+                    "Email": email,
+                    "EncryptedPasswd": GooglePlayPassword(email, password).getEncryptedPassword(),
+                    "service": "androidmarket",
+                    "accountType": self.ACCOUNT_TYPE_HOSTED_OR_GOOGLE,
+                    "has_permission": "1",
+                    "source": "android",
+                    "androidId": self.androidId,
+                    "app": "com.android.vending",
+                    # "client_sig": self.client_sig,
+                    "device_country": "us",
+                    "operatorCountry": "us",
+                    "lang": "us",
+                    "sdk_version": "17"
+                    }  # to work around oauth issues
                 headers = {
                     "Accept-Encoding": "gzip, deflate",
                 }
                 response = requests.post(self.URL_LOGIN, data=params, headers=headers, proxies=proxy, verify=False)
+
                 if response.status_code != http.client.OK:
                     logging.error('{0} Play Store login failed, statuscode {1}: {2}'.format(self.androidId, response.status_code, response.content))
                 else:
@@ -159,7 +231,8 @@ class GooglePlayAPI(object):
                     for d in data:
                         if "=" not in d:
                             continue
-                        k, v = d.split("=")
+
+                        k, v = d.split("=", 1) # Do a single split as the token is padded with `==` at the end
                         params[k.strip().lower()] = v.strip()
                     if "auth" in params:
                         self.setAuthSubToken(params["auth"])
@@ -180,19 +253,21 @@ class GooglePlayAPI(object):
         if (datapost is None and path in self.preFetch):
             data = self.preFetch[path]
         else:
-            headers = {"Accept-Language": self.lang,
-                       "Authorization": "GoogleLogin auth=%s" % self.authSubToken,
-                       "X-DFE-Enabled-Experiments": "cl:billing.select_add_instrument_by_default",
-                       "X-DFE-Unsupported-Experiments": "nocache:billing.use_charging_poller,market_emails,buyer_currency,prod_baseline,checkin.set_asset_paid_app_field,shekel_test,content_ratings,buyer_currency_in_app,nocache:encrypted_apk,recent_changes",
-                       "X-DFE-Device-Id": self.androidId,
-                       "X-DFE-Client-Id": "am-android-google",
-                       "X-DFE-Device-Config-Token": "1",
-                       "X-DFE-Cookie": base64.standard_b64encode(b'\x08\xa9\x0f\x10\x01\x18\x00"\x02' + self.regionCookie.encode('utf-8')).decode('utf-8').replace('=', ''),
-                       "User-Agent": user_agent,
-                       "X-DFE-SmallestScreenWidthDp": "320",
-                       "X-DFE-Filter-Level": "3",
-                       "Accept-Encoding": "gzip, deflate",
-                       "Host": "android.clients.google.com"}  # TODO make the values for versioncode, sdk, device, hardware, platformVersionRelease, model, isWidescreen, X-DFE-SmallestScreenWidthDp flexible?
+            headers = {
+                "Accept-Language": self.lang,
+                "Authorization": "GoogleLogin auth=%s" % self.authSubToken,
+                "X-DFE-Enabled-Experiments": "cl:billing.select_add_instrument_by_default",
+                "X-DFE-Unsupported-Experiments": "nocache:billing.use_charging_poller,market_emails,buyer_currency,prod_baseline,checkin.set_asset_paid_app_field,shekel_test,content_ratings,buyer_currency_in_app,nocache:encrypted_apk,recent_changes",
+                "X-DFE-Device-Id": self.androidId,
+                "X-DFE-Client-Id": "am-android-google",
+                "X-DFE-Device-Config-Token": "1",
+                "X-DFE-Cookie": base64.standard_b64encode(b'\x08\xa9\x0f\x10\x01\x18\x00"\x02' + self.regionCookie.encode('utf-8')).decode('utf-8').replace('=', ''),
+                "User-Agent": user_agent,
+                "X-DFE-SmallestScreenWidthDp": "320",
+                "X-DFE-Filter-Level": "3",
+                "Accept-Encoding": "gzip, deflate",
+                "Host": "android.clients.google.com"
+                }  # TODO make the values for versioncode, sdk, device, hardware, platformVersionRelease, model, isWidescreen, X-DFE-SmallestScreenWidthDp flexible?
 
             url = "https://android.clients.google.com/fdfe/%s" % path
             if datapost is not None:
@@ -291,7 +366,7 @@ class GooglePlayAPI(object):
             path += "&n=%d" % int(nb_results)
         if (offset is not None):
             path += "&o=%d" % int(offset)
-        if(filterByDevice):
+        if (filterByDevice):
             path += "&dfil=1"
         (status_code, message) = self.executeRequestApi2(path)
         if status_code == http.client.OK:
